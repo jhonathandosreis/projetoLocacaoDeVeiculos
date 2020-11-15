003ee0c6df2af320f406b5ac82b9ca338d296345
Jhonathanreis <jhonathandosreis@hotmail.com>
2020-11-15 04:28:52 -0300

    Configurando gitignore

---
+++ ProjetoMulaCar/src/br/com/pi/util/Conexao.java
@@ -0,0 +1,42 @@
+/*
+ *  --------------------------------------------------------------------------------------------------->
+ *  Licença    : MIT - Copyright 2019 Jhonathan dos reis (jhonlinux) <jhonathan.rosa@maximatech.com.br>
+ *  Criado em  : 10/10/2020 17:50:31 
+ *  Instituição: FACULDADE SENAI FATESG
+ *  Curso      : Análise e Desenvolvimento de sistemas - Módulo 3 - 2020/2
+ *  Disciplina : APS - Arquitetura e Projeto de Software
+ *  Aluno      : Jhonathan dos reis
+ *  Projeto    : PROJETO CAMADAS
+ *  Exercício  : Cadastro de miniaturas
+ *  ----------------------------------------------------------------------------------------------------
+ *  Realiza a conexão com o banco de dados.
+ *  ---------------------------------------------------------------------------------------------------| 
+ */
+package br.com.pi.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author JHONATHAN Conexão com o banco
+ */
+public class Conexao {
+    
+    private static Connection conexao = null;
+    
+    public static Connection getConexao() throws  Exception {
+        if(conexao == null){
+        
+            String driver = "org.postgresql.Driver";
+            String url = "jdbc:postgresql://13.59.235.216:5432/torrentz_filmes";
+            String user = "postgres";
+            String password = "affseilamano";
+                  
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);  
+        }
+            return conexao;
+        
+        }
+    }
