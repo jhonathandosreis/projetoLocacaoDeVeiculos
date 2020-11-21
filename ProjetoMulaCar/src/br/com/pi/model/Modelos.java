65c41ba6c69c038fe2530321b1f73d3f1287b555
Jhonathanreis <jhonathandosreis@hotmail.com>
2020-11-21 05:00:40 -0300

    Classe Modelos pronta

---
+++ ProjetoMulaCar/src/br/com/pi/model/Modelos.java
@@ -0,0 +1,77 @@
+/*
+ *  -------------------------------------------------------------------------------------------------->
+ *  Licença    : MIT - Copyright 2019 Jhonathan, Gustavo e Miguel 
+ *  Criado em  : 21/11/2020 04:57:49 
+ *  Instituição: FACULDADE SENAI FATESG
+ *  Curso      : Análise e Desenvolvimento de sistemas - Módulo 3 - 2020/11
+ *  Disciplina : Projeto Integrador
+ *  Alunos     : Jhonathan dos Reis, Gustavo Gabriel e Miguel Neto
+ *  Projeto    : Projeto Locação de Veículos
+ *  Exercício  : Mula Car
+ *  ---------------------------------------------------------------------------------------------------
+ *  Propósito do arquivo: 
+ *  ---------------------------------------------------------------------------------------------------| 
+ */
+
+package br.com.pi.model;
+
+/**
+ *
+ * @author jhonlinux
+ */
+public class Modelos {
+
+    //--- ATRIBUTOS ----------------------------------------------------------------------------------->
+    //
+    private int iden = 0;
+    private String nome = "";
+    private Marcas marcas = null;
+    //--- FIM ATRIBUTOS -------------------------------------------------------------------------------|
+    //
+
+    //--- CONSTRUTORES -------------------------------------------------------------------------------->
+    //
+    public Modelos() {
+        
+    }
+    
+    public Modelos(int iden, String nome, Marcas marcas) {
+        this.iden = iden;
+        this.nome = nome;
+        this.marcas = marcas;
+    }
+    //--- FIM CONSTRUTORES ----------------------------------------------------------------------------|
+    //
+
+    //--- GET ----------------------------------------------------------------------------------------->
+    //
+    public int getIden() {
+        return iden;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Marcas getMarcas() {
+        return marcas;
+    }    
+    //--- FIM GET -------------------------------------------------------------------------------------|
+    //
+    
+    //--- SET ----------------------------------------------------------------------------------------->
+    //
+    public void setIden(int iden) {
+        this.iden = iden;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setMarcas(Marcas marcas) {
+        this.marcas = marcas;
+    }
+    //--- FIM SET -------------------------------------------------------------------------------------|
+    //
+}
