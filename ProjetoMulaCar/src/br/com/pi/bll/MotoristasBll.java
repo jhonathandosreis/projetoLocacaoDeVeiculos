0a00e96e81481d8363f61c4b92cf2f5aad293f3e
Gustavo-404 <gustavogabriel77849@gmail.com>
2020-11-24 00:16:42 -0300

    Alteração teste

--- ProjetoMulaCar/src/br/com/pi/bll/MotoristasBll.java
+++ ProjetoMulaCar/src/br/com/pi/bll/MotoristasBll.java
@@ -32,7 +32,6 @@ public class MotoristasBll {
     //--- CONSTRUTORES -------------------------------------------------------------------------------->
     public MotoristasBll() throws Exception {
         motoristaDal = new MotoristasDal();
-        ///KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK
     }
     //--- FIM CONSTRUTORES ----------------------------------------------------------------------------|
     //
