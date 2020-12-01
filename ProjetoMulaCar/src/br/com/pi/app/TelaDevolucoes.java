3648a2b468cc631e237c9bc3fee6fdf5ffcc0a78
MiguellNeto <56120625+MiguellNeto@users.noreply.github.com>
2020-12-01 18:51:33 -0300

    dal , bll, tela devolucoes

--- ProjetoMulaCar/src/br/com/pi/app/TelaDevolucoes.java
+++ ProjetoMulaCar/src/br/com/pi/app/TelaDevolucoes.java
@@ -14,20 +14,134 @@
  */
 package br.com.pi.app;
 
+import br.com.pi.bll.DevolucoesBll;
+import br.com.pi.bll.LocacoesBll;
+import br.com.pi.model.Devolucoes;
+import br.com.pi.model.Locacoes;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author jhonlinux
  */
 public class TelaDevolucoes extends javax.swing.JFrame {
 
-    /**
-     * Creates new form TelaDevolucao
-     */
+    //--- BLL´S --------------------------------------------------------------------------------------->
+    private DevolucoesBll devolucaoBll = null;
+    private LocacoesBll locacaoBll = null;
+
+    //--- FIM BLL'S ----------------------------------------------------------------------------------->
+    //
+    //--- CLASSES -------------------------------------------------------------------------------------> 
+    private Devolucoes devolucao = null;
+    private Locacoes locacao = null;
+
+    //--- FIM CLASSES --------------------------------------------------------------------------------->
+    //
     public TelaDevolucoes() {
         initComponents();
+
+        try {
+
+            devolucao = new Devolucoes();
+            locacao = new Locacoes();
+
+            devolucaoBll = new DevolucoesBll();
+            locacaoBll = new LocacoesBll();
+
+            preencherComboboxLocacoes();
+            preencherGridDevolucoes();
+
+        } catch (Exception error) {
+            JOptionPane.showMessageDialog(null, error.getMessage(), "Menssagem", JOptionPane.ERROR_MESSAGE);
+        }
         this.setLocationRelativeTo(null);
     }
 
+    //--- METODOS ------------------------------------------------------------------------------------->
+    public void preencherGridDevolucoes() throws Exception {
+
+        try {
+            DefaultTableModel tableDevolucao = (DefaultTableModel) jTableDevolucao.getModel();
+            tableDevolucao.setRowCount(0);
+
+            Object[] linha = new Object[7];
+
+            ArrayList<Devolucoes> devolucao = new DevolucoesBll().getAllDevolucao();
+
+            for (Devolucoes devolucoes : devolucao) {
+
+                linha[0] = devolucoes.getIden();
+                linha[1] = devolucoes.getLocacao().getCliente().getNome();
+                linha[2] = devolucoes.getDataDevolucao();
+                linha[3] = devolucoes.getKmNaEntrega();
+                linha[4] = devolucoes.getStatus();
+                linha[5] = devolucoes.getMultaPorAtraso();
+                linha[6] = devolucoes.getTotalPago();  // ver como vai funiconar o pagamento , acrescentar na model e sql
+
+                tableDevolucao.addRow(linha);
+            }
+        } catch (Exception error) {
+            JOptionPane.showMessageDialog(null, error.getMessage(), "Menssagem", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public void preencherFormularioDevolucao() throws Exception {
+
+        try {
+
+            int id = Integer.parseInt(jTableDevolucao.getValueAt(jTableDevolucao.getSelectedRow(), 0).toString());
+
+            jComboBoxLocacaoCliente.setSelectedItem(locacaoBll.getLocacoesBy(id).getCliente());
+
+            String data = jTableDevolucao.getValueAt(jTableDevolucao.getSelectedRow(), 2).toString();
+
+            int kmNaEntrega = Integer.parseInt(jTableDevolucao.getValueAt(jTableDevolucao.getSelectedRow(), 3).toString());
+
+            String status = jTableDevolucao.getValueAt(jTableDevolucao.getSelectedRow(), 4).toString();
+
+            int multaPorAtraso = Integer.parseInt(jTableDevolucao.getValueAt(jTableDevolucao.getSelectedRow(), 5).toString());
+
+            double totalPago = Double.parseDouble(jTableDevolucao.getValueAt(jTableDevolucao.getSelectedRow(), 6).toString());
+
+            jTextFieldIDDevolucao.setText(id + "");
+            jTextFieldDataDevolucao.setText(data);
+            jTextFieldKmNaEntrega.setText(kmNaEntrega + "");
+            jTextFieldStatusDoVeiculo.setText(status);
+            jTextFieldMultaPorAtraso.setText(multaPorAtraso + "");
+
+        } catch (Exception error) {
+            JOptionPane.showMessageDialog(null, error.getMessage(), "Menssagem", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public void preencherComboboxLocacoes() throws Exception {
+        try {
+            jComboBoxLocacaoCliente.removeAllItems();
+            ArrayList<Locacoes> lista = locacaoBll.getAllLocacoes();
+
+            for (Locacoes locacao : lista) {
+                jComboBoxLocacaoCliente.addItem(locacao.getCliente().getNome());
+            }
+        } catch (Exception error) {
+            JOptionPane.showMessageDialog(rootPane, error.getMessage(), "Menssagem", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public void limparCampos() {
+
+        jTextFieldIDDevolucao.setText("");
+        jTextFieldDataDevolucao.setText("");
+        jTextFieldKmNaEntrega.setText("");
+        jTextFieldStatusDoVeiculo.setText("");
+        jTextFieldMultaPorAtraso.setText("");
+        jComboBoxLocacaoCliente.setSelectedIndex(0);
+    }
+
+    //--- FIM METODOS --------------------------------------------------------------------------------->
+    //
     /**
      * This method is called from within the constructor to initialize the form. WARNING: Do NOT modify
      * this code. The content of this method is always regenerated by the Form Editor.
@@ -48,6 +162,8 @@ public class TelaDevolucoes extends javax.swing.JFrame {
         jTextFieldMultaPorAtraso = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         jTextFieldStatusDoVeiculo = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        jComboBoxLocacaoCliente = new javax.swing.JComboBox<>();
         jButtonSelecionarLocacao = new javax.swing.JButton();
         jButtonFinalizar = new javax.swing.JButton();
         jButtonCancelar = new javax.swing.JButton();
@@ -71,35 +187,44 @@ public class TelaDevolucoes extends javax.swing.JFrame {
 
         jLabel4.setText("STATUS DO VEÍCULO");
 
+        jLabel5.setText("Locação / Cliente");
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addContainerGap()
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabelIdPessoaFisica)
-                            .addComponent(jTextFieldIDDevolucao, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(27, 27, 27)
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jTextFieldIDDevolucao, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addContainerGap(1053, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addComponent(jComboBoxLocacaoCliente, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGap(52, 52, 52))
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addComponent(jLabel5)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jTextFieldDataDevolucao, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(153, 153, 153)
+                        .addGap(33, 33, 33)
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jTextFieldKmNaEntrega)
-                            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addGap(101, 101, 101)
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jTextFieldMultaPorAtraso))
-                        .addGap(82, 82, 82)
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jTextFieldStatusDoVeiculo))))
-                .addContainerGap(220, Short.MAX_VALUE))
+                            .addComponent(jLabel2))
+                        .addGap(49, 49, 49)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel3)
+                            .addComponent(jTextFieldMultaPorAtraso, javax.swing.GroupLayout.PREFERRED_SIZE, 128, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(43, 43, 43)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel4)
+                            .addComponent(jTextFieldStatusDoVeiculo, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(173, 173, 173))))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -108,28 +233,37 @@ public class TelaDevolucoes extends javax.swing.JFrame {
                 .addComponent(jLabelIdPessoaFisica)
                 .addGap(14, 14, 14)
                 .addComponent(jTextFieldIDDevolucao, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(41, 41, 41)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(jLabel2)
                     .addComponent(jLabel3)
-                    .addComponent(jLabel4))
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel5))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jTextFieldDataDevolucao, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jTextFieldKmNaEntrega, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jTextFieldMultaPorAtraso, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextFieldStatusDoVeiculo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jTextFieldStatusDoVeiculo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jComboBoxLocacaoCliente, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(47, Short.MAX_VALUE))
         );
 
-        jButtonSelecionarLocacao.setIcon(new javax.swing.ImageIcon("/home/jhonlinux/Documentos/Repositorio/projetoLocacaoDeVeiculos/ProjetoMulaCar/src/br/com/pi/icons/arquivo.png")); // NOI18N
         jButtonSelecionarLocacao.setText("SELECIONAR LOCAÇÃO");
+        jButtonSelecionarLocacao.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonSelecionarLocacaoActionPerformed(evt);
+            }
+        });
 
-        jButtonFinalizar.setIcon(new javax.swing.ImageIcon("/home/jhonlinux/Documentos/Repositorio/projetoLocacaoDeVeiculos/ProjetoMulaCar/src/br/com/pi/icons/backspace.png")); // NOI18N
         jButtonFinalizar.setText("FINALIZAR");
+        jButtonFinalizar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonFinalizarActionPerformed(evt);
+            }
+        });
 
-        jButtonCancelar.setIcon(new javax.swing.ImageIcon("/home/jhonlinux/Documentos/Repositorio/projetoLocacaoDeVeiculos/ProjetoMulaCar/src/br/com/pi/icons/retroceder.png")); // NOI18N
         jButtonCancelar.setText("CANCELAR");
 
         jTableDevolucao.setModel(new javax.swing.table.DefaultTableModel(
@@ -137,15 +271,20 @@ public class TelaDevolucoes extends javax.swing.JFrame {
 
             },
             new String [] {
-                "ID", "CLIENTE", "STATUS", "DATA PREVISTA", "DATA DE ENTREGA", "TOTAL PAGO"
+                "ID", "LOCAÇÃO / CLIENTE", "DATA DA DEVOLUÇÃO", "KM NA ENTREGA", "STATUS VEÍCULO", "MULTA POR ATRASO", "TOTAL PAGO"
             }
         ) {
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, true, true
+                false, false, true, true, false, true, true
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
                 return canEdit [columnIndex];
+            }
+        });
+        jTableDevolucao.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTableDevolucaoMouseClicked(evt);
             }
         });
         jScrollPane1.setViewportView(jTableDevolucao);
@@ -183,7 +322,7 @@ public class TelaDevolucoes extends javax.swing.JFrame {
                     .addComponent(jButtonFinalizar)
                     .addComponent(jButtonCancelar))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 376, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 409, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -206,6 +345,26 @@ public class TelaDevolucoes extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButtonFinalizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonFinalizarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButtonFinalizarActionPerformed
+
+    private void jButtonSelecionarLocacaoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonSelecionarLocacaoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButtonSelecionarLocacaoActionPerformed
+
+    private void jTableDevolucaoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTableDevolucaoMouseClicked
+
+  try {
+            preencherFormularioDevolucao();
+        } catch (Exception error) {
+            JOptionPane.showMessageDialog(rootPane, error.getMessage(), "Menssagem", JOptionPane.ERROR_MESSAGE);
+        }
+
+
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTableDevolucaoMouseClicked
 
     /**
      * @param args the command line arguments
@@ -247,10 +406,12 @@ public class TelaDevolucoes extends javax.swing.JFrame {
     private javax.swing.JButton jButtonCancelar;
     private javax.swing.JButton jButtonFinalizar;
     private javax.swing.JButton jButtonSelecionarLocacao;
+    private javax.swing.JComboBox<String> jComboBoxLocacaoCliente;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabelIdPessoaFisica;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
