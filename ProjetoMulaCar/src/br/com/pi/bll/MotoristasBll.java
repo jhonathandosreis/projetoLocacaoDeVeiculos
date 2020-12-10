e3bd2b57c5936f942c469dfad63d20086822b82c
Gustavo-404 <gustavogabriel77849@gmail.com>
2020-12-10 20:03:31 -0300

    VALIDA CPF MOTORISTA

--- ProjetoMulaCar/src/br/com/pi/bll/MotoristasBll.java
+++ ProjetoMulaCar/src/br/com/pi/bll/MotoristasBll.java
@@ -12,11 +12,11 @@
  *  Propósito do arquivo: 
  *  ---------------------------------------------------------------------------------------------------| 
  */
-
 package br.com.pi.bll;
 
 import br.com.pi.dal.MotoristasDal;
 import br.com.pi.model.Motoristas;
+import br.com.pi.util.Valida;
 import java.util.ArrayList;
 
 /**
@@ -24,8 +24,9 @@ import java.util.ArrayList;
  * @author Gustavo Gabriel
  */
 public class MotoristasBll {
+
     //--- ATRIBUTOS ----------------------------------------------------------------------------------->
-    private MotoristasDal motoristaDal;  
+    private MotoristasDal motoristaDal;
     //--- FIM ATRIBUTOS -------------------------------------------------------------------------------|
     //
 
@@ -38,24 +39,37 @@ public class MotoristasBll {
 
     //--- CREATE -------------------------------------------------------------------------------------->
     public void addMotoristas(Motoristas motoristas) throws Exception {
-        try{
-         if(motoristas.getCliente().getIden() == 0) throw new RuntimeException("Erro ao inserir cliente em motorista");    
-         motoristaDal.addMotoristas(motoristas);
+        if (motoristas.getCliente().getIden() == 0) {
+            throw new RuntimeException("Erro ao inserir cliente em motorista");
+        }
+        String cpf = motoristas.getCpf();
+        cpf = cpf.replace(".", ""); //tira ponto
+        cpf = cpf.replace("-", ""); //tira hífen
+        
+        Valida.isCPF(cpf, "CPF inválido!");
+        try {
+            motoristaDal.addMotoristas(motoristas);
         } catch (Exception error) {
-            if(error.getMessage().contains("cnh_repetida")) throw new RuntimeException("Número ("+motoristas.getNumeroCnh()+") de cnh já cadastrado em nosso sistema");
-             if(error.getMessage().contains("cpf_repetido")) throw new RuntimeException("Número ("+motoristas.getCpf()+") de cpf já cadastrado em nosso sistema");
-             if(error.getMessage().contains("rg_repetido")) throw new RuntimeException("Número ("+motoristas.getRg()+") de rg já cadastrado em nosso sistema");
+            if (error.getMessage().contains("cnh_repetida")) {
+                throw new RuntimeException("Número (" + motoristas.getNumeroCnh() + ") de cnh já cadastrado em nosso sistema");
+            }
+            if (error.getMessage().contains("cpf_repetido")) {
+                throw new RuntimeException("Número (" + motoristas.getCpf() + ") de cpf já cadastrado em nosso sistema");
+            }
+            if (error.getMessage().contains("rg_repetido")) {
+                throw new RuntimeException("Número (" + motoristas.getRg() + ") de rg já cadastrado em nosso sistema");
+            }
         }
     }
     //--- FIM CREATE ----------------------------------------------------------------------------------|
     //
-    
+
     //--- UPDATE -------------------------------------------------------------------------------------->
     public void updateMotorista(Motoristas motoristas) throws Exception {
-        try{
-         motoristaDal.updateMotoristas(motoristas);
+        try {
+            motoristaDal.updateMotoristas(motoristas);
         } catch (Exception error) {
-            throw  error;
+            throw error;
         }
     }
     //--- FIM UPDATE ----------------------------------------------------------------------------------|
@@ -63,41 +77,41 @@ public class MotoristasBll {
 
     //--- DELETE -------------------------------------------------------------------------------------->
     public void deleteMotoristas(Motoristas motoristas) throws Exception {
-        try{
-         motoristaDal.deleteMotoristas(motoristas);
+        try {
+            motoristaDal.deleteMotoristas(motoristas);
         } catch (Exception error) {
-            throw  error;
+            throw error;
         }
     }
     //--- FIM DELETE ----------------------------------------------------------------------------------|
     //
-    
+
     //--- READ ---------------------------------------------------------------------------------------->
     public ArrayList<Motoristas> getAllMotoristas() throws Exception {
-        try{
-         return motoristaDal.getAllMotoristas();
+        try {
+            return motoristaDal.getAllMotoristas();
         } catch (Exception error) {
-            throw  error;
+            throw error;
         }
-        
+
     }
-    
+
     public Motoristas getMotoristaById(int mot_iden) throws Exception {
-         try{
-         return motoristaDal.getMotoristasById(mot_iden);
+        try {
+            return motoristaDal.getMotoristasById(mot_iden);
         } catch (Exception error) {
-            throw  error;
+            throw error;
         }
-        
+
     }
-    
-     public Motoristas getMotoristaBycCliente(int mot_cliente) throws Exception {
-         try{
-         return motoristaDal.getMotoristasByCliente(mot_cliente);
+
+    public Motoristas getMotoristaBycCliente(int mot_cliente) throws Exception {
+        try {
+            return motoristaDal.getMotoristasByCliente(mot_cliente);
         } catch (Exception error) {
-            throw  error;
+            throw error;
         }
-        
+
     }
     //--- FIM READ ------------------------------------------------------------------------------------|
     //   
